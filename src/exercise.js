2c6ca164a2ce93cc1c916c118d6d8a5b753c28e8
hayeong choi <hayeong28@naver.com>
2020-07-08 08:47:35 +0900

    add: type정의, 액션 생성함수

---
+++ src/exercise.js
@@ -0,0 +1,15 @@
+const INCREASE = 'Increase';
+const DECREASE = 'Decrease';
+const SET_DIFF = 'SetDiff';
+
+const increase = () => ({
+  type: INCREASE
+});
+
+const decrease = () => ({
+  type: DECREASE
+});
+
+const setDiff = () => ({
+  type: SET_DIFF
+});
