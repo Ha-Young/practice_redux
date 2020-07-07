224d9c9c628cb8e06dc8c7dcb9d7cfaa6b25f8fd
hayeong choi <hayeong28@naver.com>
2020-07-08 08:54:46 +0900

    add: reducer in counter

--- src/exercise.js
+++ src/exercise.js
@@ -1,15 +1,45 @@
-const INCREASE = 'Increase';
-const DECREASE = 'Decrease';
-const SET_DIFF = 'SetDiff';
+const INCREASE = "Increase";
+const DECREASE = "Decrease";
+const SET_DIFF = "SetDiff";
 
 const increase = () => ({
-  type: INCREASE
+  type: INCREASE,
 });
 
 const decrease = () => ({
-  type: DECREASE
+  type: DECREASE,
 });
 
 const setDiff = () => ({
-  type: SET_DIFF
+  type: SET_DIFF,
 });
+
+const initialState = {
+  value: 0,
+  diff: 1,
+};
+
+export default function counter(state = initialState, action) {
+  switch (action.type) {
+    case INCREASE:
+      return {
+        ...state,
+        value: state.value + state.diff,
+      };
+
+    case DECREASE:
+      return {
+        ...state,
+        value: state.value - state.diff,
+      };
+
+    case SET_DIFF:
+      return {
+        ...state,
+        diff: action.diff,
+      };
+
+    default:
+      return state;
+  }
+}
