14fada8e426d1fe08ef7ae156a78f494d1203602
hayeong choi <hayeong28@naver.com>
2020-07-08 09:00:08 +0900

    add: root reducer

--- src/module/init.js
+++ src/module/init.js
@@ -1,1 +1,8 @@
-import "./counter";
+import { combineReducers } from "redux";
+import counter from "./counter";
+
+const rootReducer = combineReducers({
+  counter,
+});
+
+export default rootReducer;
